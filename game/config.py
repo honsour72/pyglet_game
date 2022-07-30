08c9ebd98b016057962a99e65e9b43e436406093
Michael <honsour72@gmail.com>
2022-07-30 17:01:00 +0300

    remove stuff 95%

--- game/config.py
+++ game/config.py
@@ -10,12 +10,9 @@ from . import letter
 
 
 def player_lives(num_icons, batch=None):
-    """Generate sprites for player life icons"""
     player_lives = []
     for i in range(num_icons):
-        new_sprite = pyglet.sprite.Sprite(img=player_image,
-                                          x=785 - i * 30, y=585,
-                                          batch=batch)
+        new_sprite = pyglet.sprite.Sprite(img=player_image, x=20 + i * 30, y=555, batch=batch)
         new_sprite.scale = 0.5
         player_lives.append(new_sprite)
     return player_lives
@@ -35,8 +32,7 @@ def generate_random_words(filename: str = './resources/words.txt', amount: int =
 
 
 def get_words(words_amount: int = None, batch=None):
-    """Generate asteroid objects with random positions and velocities, not close to the player"""
-    words_list = []
+    letters = []
     words = generate_random_words(amount=words_amount)
     for word in words:
         word_x = random.randint(1, 750)
@@ -48,18 +44,16 @@ def get_words(words_amount: int = None, batch=None):
             new_letter = letter.Letter(letter=let, x=word_x, y=word_y, batch=batch)
             new_letter.velocity_y = word_velocity
             new_letter.scale = word_scale
-            words_list.append(new_letter)
+            letters.append(new_letter)
             word_y += letter_distance
-    return words_list
+    return letters
 
 
 def distance(point_1=(0, 0), point_2=(0, 0)):
-    """Returns the distance between two points"""
     return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
 
 def center_image(image):
-    """Sets an image's anchor point to its center"""
     image.anchor_x = image.width / 2
     image.anchor_y = image.height / 2
 
@@ -74,12 +68,9 @@ def get_image(letter: str) -> pyglet.resource.image:
 #                                                   ALL CONSTANTS                                                      #
 # ==================================================================================================================== #
 
-
-# Tell pyglet where to find the resources
 pyglet.resource.path = ['./resources']
 pyglet.resource.reindex()
 
-# Load the three main resources and get them to draw centered
 player_image = pyglet.resource.image("player.png")
 center_image(player_image)
 player_fire = pyglet.resource.image("player_fire.png")
@@ -91,13 +82,9 @@ center_image(bullet_image)
 x_image = pyglet.resource.image("X.png")
 center_image(x_image)
 
-# The engine flame should not be centered on the ship. Rather, it should be shown
-# behind it. To achieve this effect, we just set the anchor point outside the
-# image bounds.
 engine_image = pyglet.resource.image("engine_flame.png")
 engine_image.anchor_x = engine_image.width * 1.5
 engine_image.anchor_y = engine_image.height / 2
 
-# Load the bullet sound _without_ streaming so we can play it more than once at a time
 bullet_sound = pyglet.resource.media("bullet.wav", streaming=False)
 
--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-import pyglet
 from game import letter, config, player
 from random import randint
+import pyglet
+
 
 game_window = pyglet.window.Window(800, 600)
 main_batch = pyglet.graphics.Batch()
 score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
-level_label = pyglet.text.Label(text="Version 5: It's a Game!",
-                                x=400, y=575, anchor_x='center', batch=main_batch)
-game_over_label = pyglet.text.Label(text="GAME OVER",
-                                    x=400, y=-300, anchor_x='center',
-                                    batch=main_batch, font_size=48)
-you_win_label = pyglet.text.Label(text="CONGRATS, YOU WIN!",
-                                  x=400, y=-300, anchor_x='center',
-                                  batch=main_batch, font_size=48)
-counter = pyglet.window.FPSDisplay(window=game_window)
+game_over_label = pyglet.text.Label(text="GAME OVER", x=400, y=-300, anchor_x='center', batch=main_batch, font_size=48)
+you_win_label = pyglet.text.Label(text="YOU WIN!", x=400, y=-300, anchor_x='center', batch=main_batch, font_size=48)
 
 player_ship = None
 player_lives = []
 score = 0
 num_lives = 3
 game_objects = []
-
-# We need to pop off as many event stack frames as we pushed on
-# every time we reset the level.
 event_stack_size = 0
 
 
@@ -37,21 +28,15 @@ def start_game(only_player=False):
     for life in player_lives:
         life.delete()
 
-    # Initialize the player sprite
     player_ship = player.Player(x=400, y=300, batch=main_batch)
-
-    # Make three sprites to represent remaining lives
     player_lives = config.player_lives(num_lives, main_batch)
 
-    # Make some asteroids so we have something to shoot at
-    # asteroids = load.asteroids(num_asteroids, player_ship.position, main_batch)
     if not only_player:
         asteroids = config.get_words(words_amount=5, batch=main_batch)
         game_objects = [player_ship] + asteroids
     else:
         game_objects.append(player_ship)
 
-    # Add any specified event handlers to the event handler stack
     for obj in game_objects:
         for handler in obj.event_handlers:
             game_window.push_handlers(handler)
@@ -62,7 +47,6 @@ def start_game(only_player=False):
 def on_draw():
     game_window.clear()
     main_batch.draw()
-    counter.draw()
 
 
 def update_game(dt):
@@ -71,25 +55,20 @@ def update_game(dt):
     player_dead = False
     victory = False
 
-    # To avoid handling collisions twice, we employ nested loops of ranges.
-    # This method also avoids the problem of colliding an object with itself.
     for i in range(len(game_objects)):
         for j in range(i + 1, len(game_objects)):
 
             obj_1 = game_objects[i]
             obj_2 = game_objects[j]
 
-            # Make sure the objects haven't already been killed
             if not obj_1.dead and not obj_2.dead:
                 if obj_1.collides_with(obj_2):
                     obj_1.handle_collision_with(obj_2)
                     obj_2.handle_collision_with(obj_1)
 
-    # Let's not modify the list while traversing it
     to_add = []
 
-    # Check for win condition
-    asteroids_remaining = 0
+    letters_remaining = 0
 
     for obj in game_objects:
         obj.update(dt)
@@ -97,40 +76,29 @@ def update_game(dt):
         to_add.extend(obj.new_objects)
         obj.new_objects = []
 
-        # Check for win condition
         if isinstance(obj, letter.Letter):
-            asteroids_remaining += 1
+            letters_remaining += 1
 
-    # if asteroids_remaining == 0:
-    if score == 200:
-        # Don't act on victory until the end of the time step
+    if score == 200 or letters_remaining == 0:
         victory = True
 
-    # Get rid of dead objects
     for to_remove in [obj for obj in game_objects if obj.dead]:
         if to_remove == player_ship:
             player_dead = True
-        # If the dying object spawned any new objects, add those to the
-        # game_objects list later
         to_add.extend(to_remove.new_objects)
 
-        # Remove the object from any batches it is a member of
         to_remove.delete()
 
-        # Remove the object from our list
         game_objects.remove(to_remove)
 
-        # Bump the score if the object to remove is an asteroid
         if isinstance(to_remove, letter.Letter):
             score += 1
             score_label.text = "Score: " + str(score)
 
-    # Add new objects to the list
     game_objects.extend(to_add)
 
     # Check for win/lose conditions <-- вот тут и была ошибка
     if player_dead:
-        # We can just use the length of the player_lives list as the number of lives
         if len(player_lives) > 0:
             num_lives -= 1
             start_game(True)
@@ -138,20 +106,9 @@ def update_game(dt):
             game_over_label.y = 300
     elif victory:
         pyglet.clock.unschedule(set_words)
-        # player_ship.dead = True
         for obj in game_objects:
-
             obj.delete()
-        # player_ship.delete()
-        # player_ship.y = -200
         game_objects.clear()
-        # score += 10
-        # reset_level(len(player_lives))
-    #
-    # if score == 20:
-    #     player_ship.delete()
-        # for obj in game_objects:
-        #     obj.delete()
         you_win_label.y = 300
 
 
