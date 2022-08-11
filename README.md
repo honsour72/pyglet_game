ece37511e21c86a548f98ca63b5ff8affebe4677
Michael <honsour72@gmail.com>
2022-08-12 00:07:59 +0300

    Add comments and README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple pyglet game
+## <i>Matrix reversed-space invader</i>
+
+---- 
+_This is my simple 2D-game based on pyglet module_
+
+_Idea: space invader fights against matrix_
+
+_If you read this, please subscribe on my github account =)_ 
+
+#### Sources:
+* Pyglet Github: https://github.com/pyglet/pyglet/tree/pyglet-1.5-maintenance
+* readthedocs.io: https://pyglet.readthedocs.io/en/latest/modules/pyglet.html
+* Pyglet amazing site: https://pyglet.org/
+
+Ok, let's try it out:
+~~~
+git clone https://github.com/honsour72/pyglet_game.git
+~~~
+Enjoy!
+
--- game/player.py
+++ game/player.py
@@ -6,18 +6,32 @@ from . import bullet, physicalobject, config
 
 
 class Player(physicalobject.PhysicalObject):
+    """
+    Main player class
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialize player-base class
+        :param args:
+        :param kwargs:
+        """
         super(Player, self).__init__(img=config.player_image, *args, **kwargs)
         self.engine_sprite = pyglet.sprite.Sprite(img=config.engine_image, *args, **kwargs)
         self.engine_sprite.visible = False
-        self.thrust = 300.0
-        # self.rotate_speed = 200.0
         self.bullet_speed = 700.0
         self.reacts_to_bullets = False
         self.key_handler = key.KeyStateHandler()
         self.event_handlers = [self, self.key_handler]
 
     def update(self, dt):
+        """
+        Handle key press every dt sec
+        Change player positions via coordinates
+        Show engine sprite if key up was pressed otherwise hide it
+        :param dt: seconds
+        :return: None
+        """
         super(Player, self).update(dt)
         if self.key_handler[key.LEFT]:
             self.x -= 2
@@ -25,7 +39,7 @@ class Player(physicalobject.PhysicalObject):
             self.x += 2
 
         if self.key_handler[key.UP]:
-            self.engine_sprite.rotation = self.rotation - 90
+            self.engine_sprite.rotation = self.rotation - 90  # flame should be turned down
             self.engine_sprite.x = self.x
             self.engine_sprite.y = self.y
             self.engine_sprite.visible = True
@@ -38,16 +52,35 @@ class Player(physicalobject.PhysicalObject):
             self.y -= 2
 
     def on_key_press(self, symbol, modifiers):
+        """
+        Let player fire if space was pressed
+        Swap image on 0.2 sec
+        :param symbol: key from kb
+        :param modifiers: required parameter
+        :return: None
+        """
         if symbol == key.SPACE:
             self.image = config.player_fire
             self.fire()
-            pyglet.clock.schedule_once(self.change_image, 0.2)
+            pyglet.clock.schedule_once(self.change_image, 0.2)  # this task will execute in main loop after 0.2 sec
 
     def change_image(self, dt):
+        """
+        Changes player original image back
+        And, yes it's hard crutch!
+        :param dt: sec
+        :return: None
+        """
         self.image = config.player_image
 
     def fire(self):
-        angle_radians = math.radians(90)
+        """
+        Create a bullet objects via Bullet class
+        Set basic bullet parameters
+        Play bullet sound
+        :return: None
+        """
+        angle_radians = math.radians(90)  # bullets should fly only up on game window
         ship_radius = self.image.width / 2
         bullet_x = self.x + math.cos(angle_radians) * ship_radius
         bullet_y = self.y + math.sin(angle_radians) * ship_radius
@@ -60,5 +93,9 @@ class Player(physicalobject.PhysicalObject):
         config.bullet_sound.play()
 
     def delete(self):
+        """
+        Remove player and engine sprite
+        :return: None
+        """
         self.engine_sprite.delete()
         super(Player, self).delete()
